0d55562f0aa073e54501e89459fcbc6367a29b44
ZakharTsarapov <tsarapovs@gmail.com>
2023-08-14 13:03:19 +0300

    hw-02

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { RegistrationScreen } from './Screns/RegistrationScreen';
+import { LoginScreen } from './Screns/LoginScreen';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-700': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-400': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-500': require('./assets/fonts/Roboto-Medium.ttf'),
+  });
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <RegistrationScreen />  
+       {/* <LoginScreen /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +25,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  bgimage: {
+    flex:1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  }
 });
---
+++ Screns/PostScreen.js
@@ -0,0 +1,5 @@
+import { StyleSheet } from "react-native";
+
+export const PostsScreen = () => {};
+
+const styles = StyleSheet.create({});
